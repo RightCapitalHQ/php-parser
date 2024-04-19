baf8df61fa9cab265b4180ab612460780f76ea5f
Yilun <yilunsun11@yeah.net>
2024-04-19 13:29:20 +0800

    feat: add start and end token positions to node attributes

--- src/php-parser/types/attributes.ts
+++ src/php-parser/types/attributes.ts
@@ -3,7 +3,9 @@ import type { IComment } from './comment';
 export interface IAttributes {
   startLine: number;
   startFilePos: number;
+  startTokenPos: number;
   endLine: number;
   endFilePos: number;
+  endTokenPos: number;
   comments?: IComment[];
 }
